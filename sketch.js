f4da4728349d0b803cec323168fccc1d04291569
Codingway-Amber <amberreine7@gmail.com>
2024-09-16 14:57:42 -0500

    more practice

--- sketch.js
+++ sketch.js
@@ -2,11 +2,15 @@ function setup() {
     createCanvas(400, 400);
     background(100);
 
-    var circleSize = 30;
+    // var circleSize = 10;
     
-    for(var i = circleSize/2; i < width; i = i + circleSize*1.25){
-        ellipse(i, circleSize/2, circleSize); 
-        console.log("the value of i is now: ", i);
+    // for(var i = circleSize/2; i < width; i = i + circleSize*1.25){
+    //     ellipse(i, circleSize/2, circleSize); 
+    //     console.log("the value of i is now: ", i);
+    // }
+
+    for(var i = 0; i < 10; i++) {
+        
     }
 }
 
